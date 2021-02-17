95f9dfbc2612c9dfb1ab6549d5344a41d4248e4e
wesley guerra <guerra_ssa@hotmail.com>
2021-02-16 21:31:36 -0300

    ajuste de margem

--- src/components/About/styles.ts
+++ src/components/About/styles.ts
@@ -3,19 +3,12 @@ import styled from 'styled-components';
 export const Container_about = styled.div`
 
     max-width: 80%;
-    margin: 100px auto;
-
-    h1{
-        font-size: 2.5em;
-        color: var(--text-color);
-        margin: 24px autó;
-        text-align: center;
-    }
+    margin: 40px auto;
     p{
         font-size: 18px;
         line-height: 2;
         color: gray;
-        margin: 12px auto;
+        margin: 10px auto;
     }
 
 
